cd883770095ca9d4b8f9eeb909046b72f4788316
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-01 14:37:32 +0300

    .

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-"""
-Complex types - mixed list
+"""_summary_
 """
 from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """
-    return a sum of all nums inside a list
+    """_summary_
+
+    Args:
+        mxd_lst (List[Union[float, int]]): _description_
+
+    Returns:
+        float: _description_
     """
     return sum(mxd_lst)
