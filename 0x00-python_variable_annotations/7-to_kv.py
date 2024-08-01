4bcf01e5c52756e75272d7880e93817aa659a5f0
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-01 14:44:20 +0300

    .

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,7 +4,7 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str,float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """_summary_
 
     Args:
@@ -12,7 +12,7 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str,float]:
         v (Union[int, float]): _description_
 
     Returns:
-        tuple: _description_
+        Tuple[str,float]: _description_
     """
     return (k, v)
 
