f196056e739e74e8a03455709089c757d0396710
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-01 14:41:54 +0300

    .

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+from typing import Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple:
+    """_summary_
+
+    Args:
+        k (str): _description_
+        v (Union[int, float]): _description_
+
+    Returns:
+        tuple: _description_
+    """
+    return (k, v)
