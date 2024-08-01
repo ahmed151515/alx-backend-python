2236fffe002a0a60ee171f1210120835bb0caee4
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-01 14:26:44 +0300

    .

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,4 +5,12 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """_summary_
+
+    Args:
+        input_list (List[float]): _description_
+
+    Returns:
+        float: _description_
+    """
     return sum(input_list)
