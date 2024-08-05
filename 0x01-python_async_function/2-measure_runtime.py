c869b305879ef79fc8dc07bfe1aa576e1d808b84
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-05 18:00:40 +0300

    .

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 """_summary_
     """
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """_summary_
+
+    Args:
+        n (int): _description_
+        max_delay (int): _description_
+
+    Returns:
+        float: _description_
+    """
+    start = time.time()
+    wait_n(n, max_delay)
+    res = time.time() - start
+    return res / n
