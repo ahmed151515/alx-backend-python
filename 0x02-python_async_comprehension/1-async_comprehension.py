827ee1d773424f88c482ec24a77753ba414d06fd
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-07 00:18:13 +0300

    .

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 """_summary_
+
+    Returns:
+        _type_: _description_
     """
-import asyncio
-import random
-from typing import Generator
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """_summary_
+
+    Returns:
+        List[float]: _description_
+    """
+    return [_ async for _ in async_generator()]
