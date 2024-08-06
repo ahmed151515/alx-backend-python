b619b2036bbeb31878c7920aa0ed0b4764133484
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-07 00:16:40 +0300

    .

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 """_summary_
-    """
-import asyncio
-import random
-from typing import Generator
-
-
-async def async_generator() -> Generator[float, None, None]:
-    """_summary_
 
     Yields:
         _type_: _description_
+    """
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """_summary_
+
+    Returns:
+        AsyncGenerator[float, None]: _description_
+
+    Yields:
+        Iterator[AsyncGenerator[float, None]]: _description_
     """
     for _ in range(0, 10):
         await asyncio.sleep(1)
