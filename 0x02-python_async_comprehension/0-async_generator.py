07f3343ac7437f4b6fce7c05114bf58df178362b
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-07 00:15:01 +0300

    .

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""_summary_
+    """
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """_summary_
+
+    Yields:
+        _type_: _description_
+    """
+    for _ in range(0, 10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""_summary_
+    """
+import asyncio
+import random
+from typing import Generator
