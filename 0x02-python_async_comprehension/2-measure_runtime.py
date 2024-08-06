98267a4c04a693b7e9966cd752b6e3ac6fecf2bc
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-07 00:19:44 +0300

    .

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 """_summary_
+
+    Returns:
+        _type_: _description_
     """
 import asyncio
-import random
-from typing import Generator
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """_summary_
+
+    Returns:
+        float: _description_
+    """
+    start_time = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    return time.perf_counter() - start_time
