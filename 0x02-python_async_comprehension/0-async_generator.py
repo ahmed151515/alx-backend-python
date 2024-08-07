72385fe86b46974834a095fb99aab5fd0b29857c
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-07 10:55:18 +0300

    .

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,10 +6,10 @@
     """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """_summary_
 
     Returns:
